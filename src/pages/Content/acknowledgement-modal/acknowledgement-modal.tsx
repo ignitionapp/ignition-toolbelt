70d90c53c4e9e36d42c36c53df2ca10480cecc43
Joseph Chiang <joseph.chiang@ignitionapp.com>
2024-07-09 20:48:04 +0000

    improvement/ack-auto-refresh: [Acknowledgement] Refresh page when there is a change (fixes: #20)

--- src/pages/Content/acknowledgement-modal/acknowledgement-modal.tsx
+++ src/pages/Content/acknowledgement-modal/acknowledgement-modal.tsx
@@ -6,24 +6,41 @@ import {
   ModalHeader,
   ModalOverlay,
 } from '@chakra-ui/react';
-import React from 'react';
+import React, { useRef } from 'react';
 import { AcknowledgementList } from '../acknowledgement-list';
 
-export const AcknowledgementModal = ({
-  isOpen,
-  onClose,
-}: {
+type Props = {
   isOpen: boolean;
   onClose(): void;
-}) => {
+};
+
+export const AcknowledgementModal = ({ isOpen, onClose }: Props) => {
+  const isUpdatedRef = useRef<boolean>(false);
+
+  const handleClose = () => {
+    onClose();
+    if (isUpdatedRef.current) {
+      setTimeout(() => window.location.reload(), 500);
+    }
+  };
+
+  const handleUpdateSuccess = () => {
+    isUpdatedRef.current = true;
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="6xl" scrollBehavior="inside">
+    <Modal
+      isOpen={isOpen}
+      onClose={handleClose}
+      size="6xl"
+      scrollBehavior="inside"
+    >
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Acknowledgements</ModalHeader>
         <ModalCloseButton />
         <ModalBody paddingX="xxlarge" paddingY="large" minHeight="500px">
-          <AcknowledgementList />
+          <AcknowledgementList onUpdateSuccess={handleUpdateSuccess} />
         </ModalBody>
       </ModalContent>
     </Modal>
