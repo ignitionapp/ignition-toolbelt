930ddbd9b708a16096f8df1372c1c79bc32ffc9a
Joseph Chiang <josephj6802@gmail.com>
2024-08-28 19:05:47 +1000

    [NPE Exit] Close button not always showing (fix #44) (#45)

--- src/pages/Content/modules/set-npe-exit.ts
+++ src/pages/Content/modules/set-npe-exit.ts
@@ -1,45 +1,61 @@
+import debounce from 'debounce';
+import 'arrive'
+
 import { NPE_EXIT, q, waitForElement } from '../lib';
 
-const run = async (value = null) => {
-  const result = await chrome.storage.local.get([NPE_EXIT]);
-  const isEnabled = value ? value : result[NPE_EXIT] || false;
+const removeButton = () => {
+  const closeButtonEl = q<HTMLLinkElement>('#npe-exit-button');
+  if (closeButtonEl) {
+    closeButtonEl.parentNode?.removeChild(closeButtonEl)
+  }
+}
+
+const appendButton = async () => {
+  let closeButtonEl = q<HTMLLinkElement>('#npe-exit-button');
+  if (closeButtonEl) {
+    return
+  }
+
+  closeButtonEl = document.createElement('a') as unknown as HTMLLinkElement;
+  closeButtonEl.id = 'npe-exit-button';
+  closeButtonEl.href = '/dashboard';
+  closeButtonEl.innerHTML = '⨯';
+  closeButtonEl.title = 'Close the NPE';
+  closeButtonEl.className = 'npe-exit-button';
+
   const proposalHeaderEl = await waitForElement<HTMLDivElement>(
     '[data-testid="proposal-header"]'
   );
-
-  if (!proposalHeaderEl) {
-    return;
+  if (proposalHeaderEl?.firstChild) {
+    (proposalHeaderEl.firstChild as HTMLElement).prepend(closeButtonEl);
   }
+}
 
-  let closeButtonEl = q<HTMLLinkElement>('#npe-exit-button');
-  if (isEnabled && !closeButtonEl) {
-    const closeButtonEl = document.createElement('a');
-    closeButtonEl.id = 'npe-exit-button';
-    closeButtonEl.href = '/dashboard';
-    closeButtonEl.innerHTML = '⨯';
-    closeButtonEl.title = 'Close the NPE';
-    closeButtonEl.className = 'npe-exit-button';
-    if (proposalHeaderEl.firstChild) {
-      (proposalHeaderEl.firstChild as HTMLElement).prepend(closeButtonEl);
-    }
+const run = async (isEnabled: boolean) => {
+  if (isEnabled) {
+    appendButton()
   } else {
-    if (closeButtonEl) {
-      closeButtonEl.parentNode?.removeChild(closeButtonEl);
-    }
+    removeButton()
   }
-};
+}
 
 export const setNpeExit = () => {
   chrome.runtime.onMessage.addListener(async ({ type }) => {
-    if (type === NPE_EXIT) {
-      await run();
+    if (type !== NPE_EXIT) {
+      return
     }
+
+    const result = await chrome.storage.local.get([NPE_EXIT]);
+    const isEnabled = !!result[NPE_EXIT];
+    await run(isEnabled)
   });
 
   chrome.storage.local.onChanged.addListener(async (changes) => {
-    const featureFlags = Object.keys(changes);
-    if (featureFlags.includes(NPE_EXIT)) {
-      await run(changes[NPE_EXIT].newValue);
+    if (!changes[NPE_EXIT]) {
+      return
     }
+
+    const isEnabled = !!changes[NPE_EXIT].newValue
+    await run(isEnabled)
   });
 };
